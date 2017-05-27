b5fbbb448677aaa8a3c52cf6fbae64647d804d01
victor-iyiola <javafolabi@gmail.com>
2017-05-27 22:08:41 +0100

    Setup the Database
    There's room for improvement on the Database implementation cos a new instance of the Database class has to be created on use by any other model

---
+++ app/models/Database.php
@@ -0,0 +1,45 @@
+<?php
+
+class Database
+{
+
+  private $conn = null;
+  public $success;
+  private $status;
+
+  public function __construct()
+  {
+    try {
+      $this->conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8',
+        DB_USERNAME, DB_PASSWORD);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->success = true;
+    } catch ( PDOException $e ) {
+      $this->success = false;
+      $this->status = $e->getMessage();
+    }
+  }
+
+  public function query($query, $bindings=false)
+  {
+    try {
+      $stmt = $this->conn->prepare($query);
+      if ( $bindings ) $stmt->execute($bindings);
+      else $stmt->execute();
+      return $stmt;
+    } catch ( PDOException $e ) {
+      return false;
+    }
+  }
+
+  public function insert($table, $bindings)
+  {
+    $keys = implode(', ', array_keys($bindings));
+    $named_param = ':' . implode(', :', array_keys($bindings));
+    return $this->query("INSERT INTO $table ($keys) VALUES ($named_param)", $bindings);
+  }
+
+  public function getStatusMessage()
+  {  return $this->success ? "Connection successful!" : $this->status;  }
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'app/lib/config.php';
 require_once 'app/init.php';
 
 $app = new App;
