51f78f059e769bfe5a229a0b72db45dba4f71d00
victor-iyiola <javafolabi@gmail.com>
2017-05-28 12:56:07 +0100

    Created init file to organize code

--- app/init.php
+++ app/init.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once APP_ROOT . 'core/App.php';
-require_once APP_ROOT . 'core/Controller.php';
-require_once APP_ROOT . 'core/View.php';
-require_once APP_ROOT . 'core/Model.php';
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 5/28/17
+ * Time: 12:52 PM
+ */
+
+require_once 'app/lib/config.php';
+require_once 'app/lib/helpers/functions.php';
+require_once 'app/lib/helpers/__autoloader.php';
