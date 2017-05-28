9dbe28b1f77691451499173857e5e2d7c9d51b04
victor-iyiola <javafolabi@gmail.com>
2017-05-28 11:54:06 +0100

    Implemented autoloader

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'app/lib/config.php';
-//require_once 'app/lib/helpers/__autoloader.php';
-require_once 'app/init.php';
+require_once 'app/lib/helpers/__autoloader.php';
+//require_once 'app/init.php';
 
 $app = new App\Core\App;
